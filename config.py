a5b3d977729b58356d26b0fcbaf00e5ac973bbfe
SteveElouga <nyobeelouga5@gmail.com>
2024-07-07 08:40:18 +0100

    Maintenance

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+# from datetime import timedelta
 import os
+# import random
+# import string
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
-print(SQLALCHEMY_DATABASE_URI)
+JWT_SECRET_KEY = ">hi8uOzhk20/C'mX\x0c[]X9Vd:"
+# JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+# JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
